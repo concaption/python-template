1fdcb26ebd1c1294ddbe24df304582bd57ffb036
Usama Navid <concaption@gmail.com>
2023-09-14 00:51:37 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,12 @@
 
 #create a virtualenv
 python -m venv .venv
-# source virtualenv
-source .venv/bin/activate
+
 # append it to bash so every shell launches with it 
 echo 'source .venv/bin/activate' >> ~/.bashrc
+
+# source virtualenv
+source .venv/bin/activate
+
 # install all software 
 make install
